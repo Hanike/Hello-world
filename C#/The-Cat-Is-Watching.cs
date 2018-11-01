717a6a21271ad39411d2fe95b2143ccd26e80d08
Hanike Treisce <hanike@gmail.com>
2018-11-01 01:27:47 -0300

    Create The-Cat-Is-Watching.cs
    Accept The Call of Cathulhu

---
+++ C#/The-Cat-Is-Watching.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace MeowWorld
+{
+	public class Program
+	{
+		public static void Main()
+		{
+      // The Presence
+			Console.WriteLine("There's something hidden in that dark corner...");
+			Console.ReadLine();
+			Console.WriteLine("A presence that silently observes you...");
+			Console.ReadLine();
+			Console.WriteLine("You can't run away from it...");
+			Console.ReadLine();
+			Console.WriteLine("The presence follows all your moves...");
+			Console.ReadLine();
+			
+      // Remember The Cat
+			Console.WriteLine("Stop trying, it's everywhere...");
+			Console.ReadLine();
+			Console.WriteLine("Just remember: ");
+			Console.ReadLine();
+			Console.WriteLine("...there's always a cat watching you from the darkness...");
+			Console.ReadLine();
+			Console.WriteLine("Accept the meow and press [Enter] to exit this Terminal!");
+			Console.ReadLine();
+		}
+	}
+}
